aa38d34ace82cd269796b1fd64e6431c810fdb57
Jennifer Liebel <jenniferliebel@t-online.de>
2014-04-30 21:54:14 +0200

    Python files hinzugefuegt

---
+++ Abgabe3/handler.py
@@ -0,0 +1,18 @@
+import signal
+import time
+
+def main():
+    while(True):
+	    time.sleep(5)
+    
+def handler1(signum,frame):
+    print "Signal angekommen: SIGINT!"
+
+def handler2(signum,frame):
+    print "Signal angekommen: SIGQUIT!"
+    
+    
+signal.signal(signal.SIGINT, handler1)
+signal.signal(signal.SIGQUIT, handler2)
+
+main()
